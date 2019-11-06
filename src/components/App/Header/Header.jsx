9fa8f9621717f9bd0a71eefb821908357a916ee2
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-11-06 15:45:51 +0100

    Add navbar

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import { 
     Navbar,
     NavbarBrand,
@@ -6,20 +7,56 @@ import {
     NavItem,
     Nav
 } from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import NavLink from './NavLink/NavLink'
+
+library.add(fab)
 
 export default class Header extends Component {
   render() {
     return (
       <div>
-        <Navbar color="light" >
+        <Navbar color="light" light expand="md">
             <NavbarBrand>
-                Aquapark
+                <Link to="/">Aquapark</Link>
             </NavbarBrand>
-            <Collapse isOpen={true} navbar>
+            <Collapse navbar>
+                <Nav className="mx-auto" navbar>
+                    <NavItem>
+                        <NavLink exact to="/atrakcje">Atrakcje</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="/cennik">Cennik</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="/karnet">Karnet</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink exact to="/o-nas">O nas</NavLink>
+                    </NavItem>
+                </Nav>
                 <Nav navbar>
                     <NavItem>
-                        <NavLink exact to="/">Strona główna</NavLink>
+                        <NavLink to="#">
+                            <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="#">
+                            <FontAwesomeIcon icon={['fab', 'google']} />
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="#">
+                            <FontAwesomeIcon icon={['fab', 'instagram']} />
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="#">
+                            <FontAwesomeIcon icon={['fab', 'youtube']} />
+                        </NavLink>
                     </NavItem>
                 </Nav>
             </Collapse>
