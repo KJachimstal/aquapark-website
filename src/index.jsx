e967bf51c6c503d9ad372b645123e1006f216040
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-11-06 12:51:31 +0100

    Added bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-import './assets/sass/index.sass';
+import './assets/scss//bootstrap/custom.scss';
+import 'bootstrap/scss/bootstrap.scss';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
