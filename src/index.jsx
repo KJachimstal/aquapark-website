76a2eb2b86bd1d4fe81192f8970f9f595c8b3a66
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-11-06 12:33:05 +0100

    Add node sass package

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import './assets/sass/index.sass';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
