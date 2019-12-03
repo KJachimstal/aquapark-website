f05ac220b89f500e54c161dcf511d784edcc25ca
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-12-03 14:39:19 +0100

    Change menu color and background img

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import HomeBackground from '../../assets/images/home-background.png';
+import HomeBackground from '../../assets/images/Baltic-Park-Molo-Aquapark.jpg';
+// import HomeBackground from '../../assets/images/home-background.png';
 
 const Home = () => (
   <div className="hero" style={{ backgroundImage: `url(${HomeBackground})` }}>
-    <div className="hero__content"></div>
+    <div className="hero__content">
+      <div className="container-fluid">Jakiś teskt żeby sprawdzić.</div>
+    </div>
   </div>
 );
 
