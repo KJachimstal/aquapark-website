8f2b5d455a6a7d1c0d0bd011ba3e6e621967bc00
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-12-04 13:13:35 +0100

    Initialize footer component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import NotFound from '../NotFound';
 import Header from './Header';
+import Footer from './Footer';
 
 const App = () => (
   <Router>
@@ -11,6 +12,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
+    <Footer />
   </Router>
 );
 
