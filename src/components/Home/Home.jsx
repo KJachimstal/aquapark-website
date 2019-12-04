319bf8294fa3d15f5c2b06760004d4ebedb302f7
Konrad Jachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-12-04 12:48:44 +0100

    Create hero (#3)
    * Added hero scheme
    
    * Change menu color and background img
    
    * Update logo
    
    * Update hero image
    
    * Update menu item styles
    
    * Update menu item animations
    
    * Add responsive hero items

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,52 @@
 import React from 'react';
-import HomeBackground from '../../assets/images/home-background.png';
+import { Link } from 'react-router-dom';
+
+import HomeBackground from '../../assets/images/hero-home.jpg';
+import SwimmingSchoolImage from '../../assets/images/home/swimming-school.jpg';
+import MobilePaymentImage from '../../assets/images/home/mobile-payment.jpg';
+import SwimmingPool from '../../assets/images/home/swimming-pool.jpg';
+// import HomeBackground from '../../assets/images/home-background.png';
 
 const Home = () => (
-  <div>
-    <img src={HomeBackground} className="img-fluid" alt=""/>
-    <div className="container-fluid">
-      <div className="display-4">Lorem ipsum dolor sit amet</div>
-      <h1>Lorem ipsum dolor sit amet</h1>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget dignissim diam, ut scelerisque neque. Duis a velit vestibulum, ornare dui sit amet, finibus arcu. Nam et lectus vel elit tincidunt tincidunt et ac dolor. Aliquam vel ex eu nisl volutpat vestibulum non ac orci. Fusce a leo ac arcu ullamcorper egestas eu eu enim. Nullam eget nulla imperdiet massa sagittis bibendum eu a purus. Integer quis neque nec mauris pretium semper a pellentesque neque. Nunc finibus, tortor sit amet imperdiet imperdiet, orci mi cursus ipsum, et rutrum purus ante id nunc. Morbi vestibulum neque enim, eget semper odio facilisis et. Donec sed libero a augue pharetra eleifend. Sed malesuada nulla non orci facilisis sagittis. Aliquam ac placerat ipsum. Maecenas accumsan, urna vel dapibus elementum, turpis sapien vehicula velit, ut tincidunt orci nibh molestie lorem. Curabitur blandit iaculis est quis euismod.
-      </p>
-      <h2>Lorem ipsum dolor sit amet</h2>
-      <p>
-Nam ultricies metus vel commodo interdum. Curabitur felis dui, suscipit eget sagittis vel, accumsan sed magna. Aliquam aliquet laoreet ipsum ut dictum. Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque iaculis vitae leo vitae dapibus. Maecenas euismod sem sit amet mauris blandit, nec lacinia lorem elementum. Sed elementum convallis urna ut ornare.
-      </p>
-      <h3>Lorem ipsum dolor sit amet</h3>
-      <p>
-Suspendisse at vestibulum felis. Mauris placerat eget turpis nec suscipit. Curabitur id porta ante, eget euismod sem. Praesent risus purus, aliquam sit amet est vel, tincidunt tristique enim. Suspendisse potenti. Pellentesque efficitur laoreet neque, et rhoncus nibh dignissim ac. Curabitur porttitor leo vitae eros suscipit semper. Nam at semper metus, in dapibus lectus. Donec non est eget nisl tincidunt convallis.
-      </p>
-      <h4>Lorem ipsum dolor sit amet</h4>
-      <p>
-Phasellus fermentum sagittis purus id condimentum. Aliquam condimentum vestibulum sem non eleifend. Donec viverra elementum magna. Etiam blandit purus leo, nec tempor sem egestas non. Fusce scelerisque mattis orci, cursus tincidunt mi pellentesque eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean lobortis urna in tortor viverra euismod. Maecenas pellentesque sed felis tempor suscipit.
-      </p>
-      <p>
-Curabitur faucibus rutrum dui, at dignissim quam euismod eget. Vivamus id risus maximus, elementum nibh sit amet, ultricies augue. Aenean tincidunt massa non felis porttitor consequat. Fusce lacinia magna vel aliquet scelerisque. Nullam pharetra libero facilisis, ultricies lorem eget, ornare augue. Maecenas lobortis ullamcorper est at sollicitudin. Vestibulum egestas ut purus a luctus. Fusce suscipit nisi at gravida ultricies. In ac pharetra lacus.
-      </p>
-    </div>
+  <div className="hero" style={{ backgroundImage: `url(${HomeBackground})` }}>
+    <div className="hero__content">
+      <div className="container hero__container">
+        <div className="row align-items-stretch">
+          <div className="col-7 hero__column">
+            <Link to="/bilety" className="hero__item hero__item--main">
+              <div className="hero__image">
+                <img src={SwimmingPool} alt=""/>
+              </div>
+              <header className="hero__header hero__header--big">Ferie w Aquaparku</header>
+              <div className="hero__body">
+                Oszczędzaj czas dzięki możliwości zakupu biletu online
+              </div>
+            </Link>
+          </div>
+          <div className="col-5 hero__column">
+            <Link to="/bilety" className="hero__item hero__item--addition">
+              <div className="hero__image">
+                <img src={MobilePaymentImage} alt=""/>
+              </div>
+              <header className="hero__header hero__header--medium">Kupuj online</header>
+              <div className="hero__body">
+                Oszczędzaj czas dzięki możliwości zakupu biletu online
+              </div>
+            </Link>
+            <Link to="/szkola-plywania" className="hero__item hero__item--addition">
+              <div className="hero__image">
+                <img src={SwimmingSchoolImage} alt=""/>
+              </div>
+              <header className="hero__header hero__header--medium">Szkoła pływania</header>
+              <div className="hero__body">
+                Przez cały sierpień będziemy sukcesywnie dodawać nowe grupy
+              </div>
+            </Link>
+          </div>
+        </div>
 
+      </div>
+    </div>
   </div>
 );
 
