f6533e6e0c181b4f6f1390b3d0776776ec806f84
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-12-05 15:05:25 +0100

    Add PricingItem

--- src/components/scenes/Pricing/Pricing.jsx
+++ src/components/scenes/Pricing/Pricing.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Hero from '../../modules/Hero';
 import PricingBackground from '../../../assets/images/hero-pricing.jpg';
+import pricing from '../../../data/pricing';
+import PricingItem from './PricingItem/PricingItem';
 
 const Pricing = () => (
   <>
@@ -8,8 +10,7 @@ const Pricing = () => (
     <div className="pricing">
       <div className="pricing__container container">
         <div className="row">
-          <div className="col-12 col-md-6 col-lg-3">
-          </div>
+          {pricing.map(item => <PricingItem key={item.title} {...item} />)}
         </div>
       </div>
     </div>
--- src/components/scenes/Pricing/PricingItem/PricingItem.jsx
+++ src/components/scenes/Pricing/PricingItem/PricingItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const PricingItem = ({ title }) => (
-  <div className="pricing__item">
-    <div className="pricing__price">
-      <div className="pricing__price-value">49</div>
-      <div className="pricing__price-currency">PLN</div>
+  <div className="col-12 col-md-6 col-lg-3">
+    <div className="pricing__item">
+      <div className="pricing__price">
+        <div className="pricing__price-value">49</div>
+        <div className="pricing__price-currency">PLN</div>
+      </div>
+      <div className="pricing__title">{title}</div>
+      <div className="pricing__content">dla studenta</div>
     </div>
-    <div className="pricing__title">{title}</div>
-    <div className="pricing__content">dla studenta</div>
   </div>
 );
 
