ffa7d336927e93ec77366514f5b65621bd47e76b
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-12-05 12:53:27 +0100

    Add Pricing page structure

---
+++ src/components/scenes/Pricing/Pricing.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Hero from '../../modules/Hero';
+import PricingBackground from '../../../assets/images/hero-pricing.jpg';
+
+const Pricing = () => (
+  <Hero image={PricingBackground}></Hero>
+);
+
+export default Pricing;
