4bd0af781223efc6f96e7ef75e8ac44d8c1dcae9
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-12-10 13:13:51 +0100

    Correction

--- src/components/main/App/App.jsx
+++ src/components/main/App/App.jsx
@@ -4,7 +4,7 @@ import Home from '../../scenes/Home';
 import NotFound from '../../scenes/NotFound';
 import Header from '../../modules/Header';
 import Footer from '../../modules/Footer';
-import Pricing from '../../scenes/Pricing/Pricing';
+import Pricing from '../../scenes/Pricing';
 
 const App = () => (
   <Router>
