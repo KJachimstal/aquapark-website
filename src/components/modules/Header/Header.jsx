b915fe0a39fcab087959bb1bd2c745f7f5d19453
Patryk Janicki <kontakt.janicki@gmail.com>
2019-12-23 14:11:37 +0100

    Add header scroll effect (#7)
    * Add header scroll effect
    
    * Add commas to header component
    
    * Fix footer position
    
    * Add payment providers in pricing scene

--- src/components/modules/Header/Header.jsx
+++ src/components/modules/Header/Header.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Navbar, NavbarBrand, Collapse, NavItem, Nav,
 } from 'reactstrap';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -11,65 +12,72 @@ import Logo from '../../../assets/images/logo.png';
 
 library.add(fab);
 
-const Header = () => (
-  <Navbar dark expand="md" className="header">
-    <div className="container">
-      <NavbarBrand tag={Link} to="/">
-        <img src={Logo} alt="" />
-      </NavbarBrand>
-      <Collapse navbar>
-        <Nav className="mx-auto menu" navbar>
-          <NavItem className="menu__item">
-            <NavLink exact to="/atrakcje">
-              Atrakcje
-            </NavLink>
-          </NavItem>
-          <NavItem className="menu__item">
-            <NavLink exact to="/cennik">
-              Cennik
-            </NavLink>
-          </NavItem>
-          <NavItem className="menu__item">
-            <NavLink exact to="/karnet">
-              Karnet
-            </NavLink>
-          </NavItem>
-          <NavItem className="menu__item menu__item--highlight">
-            <NavLink exact to="/o-nas">
-              Bilety
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <Nav navbar className="social-menu">
-          <NavItem className="social-menu__item">
-            <NavLink to="#">
-              <FontAwesomeIcon icon={['fab', 'facebook-f']} />
-            </NavLink>
-          </NavItem>
-          <NavItem className="social-menu__item">
-            <NavLink to="#">
-              <FontAwesomeIcon icon={['fab', 'google']} />
-            </NavLink>
-          </NavItem>
-          <NavItem className="social-menu__item">
-            <NavLink to="#">
-              <FontAwesomeIcon icon={['fab', 'instagram']} />
-            </NavLink>
-          </NavItem>
-          <NavItem className="social-menu__item">
-            <NavLink to="#">
-              <FontAwesomeIcon icon={['fab', 'youtube']} />
-            </NavLink>
-          </NavItem>
-          <NavItem className="social-menu__item">
-            <NavLink to="#">
-              <FontAwesomeIcon icon={['fab', 'tripadvisor']} />
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </div>
-  </Navbar>
-);
+const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+  useScrollPosition(({ currPos }) => {
+    setScrolled(currPos.y < 0);
+  });
+
+  return (
+    <Navbar dark expand="md" className={`header${scrolled ? ' header--scrolled' : ''}`}>
+      <div className="container">
+        <NavbarBrand tag={Link} to="/">
+          <img src={Logo} alt="" />
+        </NavbarBrand>
+        <Collapse navbar>
+          <Nav className="mx-auto menu" navbar>
+            <NavItem className="menu__item">
+              <NavLink exact to="/atrakcje">
+                Atrakcje
+              </NavLink>
+            </NavItem>
+            <NavItem className="menu__item">
+              <NavLink exact to="/cennik">
+                Cennik
+              </NavLink>
+            </NavItem>
+            <NavItem className="menu__item">
+              <NavLink exact to="/karnet">
+                Karnet
+              </NavLink>
+            </NavItem>
+            <NavItem className="menu__item menu__item--highlight">
+              <NavLink exact to="/o-nas">
+                Bilety
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <Nav navbar className="social-menu">
+            <NavItem className="social-menu__item">
+              <NavLink to="#">
+                <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+              </NavLink>
+            </NavItem>
+            <NavItem className="social-menu__item">
+              <NavLink to="#">
+                <FontAwesomeIcon icon={['fab', 'google']} />
+              </NavLink>
+            </NavItem>
+            <NavItem className="social-menu__item">
+              <NavLink to="#">
+                <FontAwesomeIcon icon={['fab', 'instagram']} />
+              </NavLink>
+            </NavItem>
+            <NavItem className="social-menu__item">
+              <NavLink to="#">
+                <FontAwesomeIcon icon={['fab', 'youtube']} />
+              </NavLink>
+            </NavItem>
+            <NavItem className="social-menu__item">
+              <NavLink to="#">
+                <FontAwesomeIcon icon={['fab', 'tripadvisor']} />
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </div>
+    </Navbar>
+  );
+};
 
 export default Header;
