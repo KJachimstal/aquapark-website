518f0222c5d643a9a7fdda386ba69a53c42fec71
Patryk Janicki <kontakt.janicki@gmail.com>
2019-12-23 15:05:22 +0100

    Add tickets background

--- src/components/scenes/Tickets/Tickets.jsx
+++ src/components/scenes/Tickets/Tickets.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import TicketsBackground from '../../../assets/images/hero-tickets.jpg';
 
 const Tickets = () => {
   return (
     <>
-      qasd
+      <div className="hero hero--small" style={{ backgroundImage: `url(${TicketsBackground})` }}>
+        <div className="hero__content hero__content--header">
+          <div className="container">
+            <div className="display-4">Bilety</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
