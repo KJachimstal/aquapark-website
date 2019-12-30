8308605f40266cdf1abfbb3dea6fdd5181975b5b
Patryk Janicki <kontakt.janicki@gmail.com>
2019-12-30 12:16:45 +0100

    Add not found page

--- src/components/scenes/NotFound/NotFound.jsx
+++ src/components/scenes/NotFound/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <div>Not Found!</div>
+  <div className="not-found">
+    <div className="not-found__content">
+      <h1>Nie odnaleziono strony</h1>
+      <p>Strona, której szukasz nie istnieje lub została przeniesiona.</p>
+      <Link to="/" className="btn btn-outline-light">Powrót do strony głównej</Link>
+    </div>
+  </div>
 );
 
 export default NotFound;
