8b3d5ba75bbef312018444d421a48666f6eceadd
Patryk Janicki <kontakt.janicki@gmail.com>
2019-12-30 11:49:45 +0100

    Add responsive styles to tickets

--- src/components/scenes/Tickets/Ticket/Ticket.jsx
+++ src/components/scenes/Tickets/Ticket/Ticket.jsx
@@ -3,15 +3,15 @@ import { Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
-const Ticket = ({ title, price, onAdd }) => (
+const Ticket = ({ title, price }) => (
   <div className="cart__item row">
-    <div className="cart__title col-6">{title}</div>
-    <div className="cart__price col-4">
+    <div className="cart__title col-12 col-md-6 text-center text-md-left py-2 py-md-0">{title}</div>
+    <div className="cart__price col-8 col-md-4">
       <span className="cart__price-value">{price}</span>
       <span className="cart__price-currency">PLN</span>
     </div>
-    <div className="col-2 text-right">
-      <Button color="primary" onClick={() => onAdd(price)}>
+    <div className="col-4 col-md-2 text-right">
+      <Button color="primary">
         <FontAwesomeIcon icon={faCartPlus} />
       </Button>
     </div>
